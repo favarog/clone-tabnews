1b8979d445eeee49ab5ebd127cc44b9adc57e739
Guilherme Favaro <10260386+favarog@users.noreply.github.com>
2024-05-03 00:03:45 +0000

    add a new test to `/migrations` endpoint about the fix

--- tests/integration/api/v1/migrations/get.test.js
+++ tests/integration/api/v1/migrations/get.test.js
@@ -15,3 +15,16 @@ test("Get to /api/v1/migrations should return 200", async () => {
   expect(Array.isArray(responseBody)).toBe(true);
   expect(responseBody.length).toBeGreaterThan(0);
 });
+
+test("Other methods to /api/v1/migrations should return 405", async () => {
+  const responseMigrations = await fetch(
+    "http://localhost:3000/api/v1/migrations",
+    { method: "DELETE" },
+  );
+  expect(responseMigrations.status).toBe(405);
+
+  const response = await fetch("http://localhost:3000/api/v1/status");
+  const responseBody = await response.json();
+
+  expect(responseBody.dependencies.database.opened_connections).toEqual(1);
+});
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -9,13 +9,5 @@ test("Get to /api/v1/status should return 200", async () => {
 
   expect(responseBody.dependencies.database.version).toEqual("16.0");
   expect(responseBody.dependencies.database.max_connections).toEqual(100);
-
-  const responseMigrations = await fetch(
-    "http://localhost:3000/api/v1/migrations",
-    {
-      method: "DELETE",
-    },
-  );
-
   expect(responseBody.dependencies.database.opened_connections).toEqual(1);
 });
